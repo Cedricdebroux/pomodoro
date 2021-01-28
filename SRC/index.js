29d3f6deffc104514ab9cfade5bc4da2273efc52
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-28 12:24:34 +0100

    import files css

--- SRC/index.js
+++ SRC/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./style.css";
 class HelloMessage extends React.Component {
     render() {
         return (
             <div>
-                {"Hello"} {this.props.name}
+                <h1>
+                    {"Hello"} {this.props.name}
+                </h1>
             </div>
         );
     }
