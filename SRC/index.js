abe90deb8c92f60a92af86670081e8dca2fae3cd
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-29 19:19:51 +0100

    up down reset

--- SRC/index.js
+++ SRC/index.js
@@ -5,38 +5,40 @@ import "./style.css";
 function useIncrement(initial, step) {
     const [count, setCount] = useState(initial);
     const increment = () => {
-        setCount(c => c + step);
+        if (count < 90) {
+            setCount(c => c + step);
+        }
     };
-    return [count, increment];
+    const decrement = () => {
+        if (count > 0) {
+            setCount(c => c - step);
+        }
+    };
+    const reset = () => {
+        setCount(20);
+    };
+    return [count, increment, decrement, reset];
 }
-function Compteur() {
-    const [count, increment] = useIncrement(0, 1);
+function Count() {
+    const [count, increment, decrement, reset] = useIncrement(20, 1);
     return (
         <>
             {count}
             <button onClick={increment} type={"button"}>
                 {`UP`}
             </button>
+            <button onClick={decrement} type={"button"}>
+                {`down`}
+            </button>
+            <button onClick={reset} type={"button"}>
+                {`reset`}
+            </button>
         </>
     );
 }
 render(
     <div>
-        <Compteur />
+        <Count />
     </div>,
     document.querySelector(`#app`),
 );
-/*class HelloMessage extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>
-                    {"Hello"} {this.props.name}
-                </h1>
-            </div>
-        );
-    }
-}
-
-const mountNode = document.querySelector("#app");
-ReactDOM.render(<HelloMessage name={"Cedric"} />, mountNode);*/
