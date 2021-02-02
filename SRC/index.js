3934f7a94341591237c29be6aae37bb25c070cdf
Debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-02-02 16:01:44 +0100

    clean src directory

--- SRC/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./app";
-
-import "./app.css";
-
-const mountNode = document.querySelector("#app");
-ReactDom.render(<App />, mountNode);
